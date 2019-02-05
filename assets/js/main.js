9f814c50c547344850a7b599d7fe67e337cc4be9
stefanninkov <40472637+stefanninkov@users.noreply.github.com>
2019-02-05 14:59:22 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+// function change() {
+// 	document.getElementById('show').innerHTML = document.getElementById('hidden').innerHTML;
+// };
+
+// Gallery.
+			$('#gallery').each(function() {
+
+				var	$gallery = $(this),
+					$content = $gallery.find('.content');
+
+				// Tabs.
+					$gallery.each( function() {
+
+						var $this = $(this),
+							$tabs = $this.find('.tabs a'),
+							$media = $this.find('.media');
+
+						$tabs.on('click', function(e) {
+
+							var $this = $(this),
+								tag = $this.data('tag');
+
+							// Prevent default.
+							 	e.preventDefault();
+
+							// Hide media that do not have the same class as the clicked tab.
+								$media
+									.fadeOut('fast')
+									.each(function() {
+
+										var $this = $(this);
+
+										if ($this.hasClass(tag))
+											$this
+												.fadeOut('fast')
+												.delay(200)
+												.queue(function(next) {
+													$this.fadeIn();
+													next();
+												});
+
+									});
+
+						});
+
+					});
+
+
+			
+				});
